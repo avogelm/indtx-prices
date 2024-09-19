af663eda224c4aeb4a1872e2cea1a50864025636
Alejandro Vogel <alejandro@MacBook-Pro-de-Alejandro.local>
2024-09-19 22:53:05 +0200

    initial commit

---
+++ src/main/java/com/avogelm/indtxprices/IndtxPricesApplication.java
@@ -0,0 +1,13 @@
+package com.avogelm.indtxprices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndtxPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndtxPricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avogelm/indtxprices/IndtxPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avogelm.indtxprices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndtxPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
