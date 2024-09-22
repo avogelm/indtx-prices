40ce4b821775e527113143b6f50caceaa6885649
alejandro <avogelm@gmail.com>
2024-09-22 11:47:22 +0200

    exception handling custom messages

--- src/main/java/com/avogelm/indtxprices/application/core/errors/IndtxPricesControllerAdvice.java
+++ src/main/java/com/avogelm/indtxprices/application/core/errors/IndtxPricesControllerAdvice.java
@@ -3,7 +3,6 @@ package com.avogelm.indtxprices.application.core.errors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,79 +16,51 @@ public class IndtxPricesControllerAdvice {
 
     private static final Logger logger = LoggerFactory.getLogger(IndtxPricesControllerAdvice.class);
 
-    @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundException(Exception e) {
-
-        logger.error(e.getMessage());
-
-        HttpStatus status = HttpStatus.NOT_FOUND;
-
+    private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String message) {
         return new ResponseEntity<>(
-                new ErrorResponse(
-                        status,
-                        e.getMessage()
-                ),
-                HttpStatusCode.valueOf(status.value())
+                new ErrorResponse(status, message),
+                status
         );
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NoResourceFoundException e) {
+        logger.error(e.getMessage());
+        return buildErrorResponse(HttpStatus.NOT_FOUND, "The requested resource could not be found.");
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<ErrorResponse> handleMissingParamException(Exception e) {
-
+    public ResponseEntity<ErrorResponse> handleMissingParamException(MissingServletRequestParameterException e) {
         logger.error(e.getMessage());
-
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-
-        return new ResponseEntity<>(
-                new ErrorResponse(
-                        status,
-                        e.getMessage()
-                ),
-                HttpStatusCode.valueOf(status.value())
-        );
+        String message = String.format("The parameter '%s' is missing.", e.getParameterName());
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, message);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<ErrorResponse> handleControllerTypeErrorException(Exception e) {
-
+    public ResponseEntity<ErrorResponse> handleControllerTypeErrorException(MethodArgumentTypeMismatchException e) {
         logger.error(e.getMessage());
-
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-
-        return new ResponseEntity<>(
-                new ErrorResponse(
-                        status,
-                        e.getMessage()
-                ),
-                HttpStatusCode.valueOf(status.value())
-        );
+        String message = String.format("Parameter '%s' should be of type '%s'.",
+                e.getName(), e.getRequiredType().getSimpleName());
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, message);
     }
 
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<ErrorResponse> handleHttpErrorException(Exception e) {
-
         logger.error(e.getMessage());
 
         ResponseStatusException exception = (ResponseStatusException) e;
-        HttpStatus status = HttpStatus.valueOf(exception.getStatusCode().value());
-
-        return new ResponseEntity<>(
-                new ErrorResponse(
-                        status,
-                        exception.getReason()
-                ),
-                exception.getStatusCode()
+        return buildErrorResponse(
+                HttpStatus.valueOf(exception.getStatusCode().value()),
+                exception.getReason()
         );
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleMiscErrorException(Exception e) {
-
         logger.error(e.getMessage());
-
-        return new ResponseEntity<>(
-                new ErrorResponse(),
-                HttpStatus.INTERNAL_SERVER_ERROR
+        return buildErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "An unexpected error occurred"
         );
     }
 }
